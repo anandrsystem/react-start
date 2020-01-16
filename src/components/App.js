fa96cd9afe65fd77e565e91fc62ac3b190b79d68
Anand M <anand.mittal@rsystems.com>
2020-01-16 11:46:07 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useParams,
+  useRouteMatch
+} from "react-router-dom";
+
+const App = () => (
+  <div>Hello world !!</div>
+)
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/App';
+// import rootReducer from './reducers';
+
+// //intialize store
+// // const createStoreWithMiddleware = applyMiddleware()(createStore);
+// // const store = createStoreWithMiddleware(rootReducer)
+// const store = createStore(rootReducer);
+// // console.log(store.getState());
+
+ReactDOM.render(<App />, document.getElementById('App')
+//   <Provider store={store}>
+//     <App />
+//   </Provider>, document.getElementById('App')
+)
